cf4535a01def4deef461402915eedd928c7120e1
Slim Ben Tanfous <111870480+SlimBenTanfous1@users.noreply.github.com>
2024-07-17 18:18:05 +0100

    push for 3

--- src/main/java/tn/bfpme/controllers/EmployeController.java
+++ src/main/java/tn/bfpme/controllers/EmployeController.java
@@ -3,36 +3,33 @@ package tn.bfpme.controllers;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.stage.Popup;
-import javafx.stage.Stage;
-import javafx.stage.Window;
 import tn.bfpme.models.*;
 import tn.bfpme.services.ServiceConge;
 import tn.bfpme.services.ServiceUserSolde;
 import tn.bfpme.utils.MyDataBase;
 import tn.bfpme.utils.SessionManager;
-import tn.bfpme.utils.StageManager;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class EmployeController implements Initializable {
@@ -49,14 +46,6 @@ public class EmployeController implements Initializable {
     @FXML
     private AnchorPane MainAnchorPane;
     @FXML
-    private Label CU_ANL;
-    @FXML
-    private Label CU_EXP;
-    @FXML
-    private Label CU_MAL;
-    @FXML
-    private Label CU_MAT;
-    @FXML
     private TableView<Conge> TableHistorique;
     @FXML
     private TableColumn<Conge, LocalDate> TableDD;
@@ -66,8 +55,12 @@ public class EmployeController implements Initializable {
     private TableColumn<Conge, TypeConge> TableType;
     @FXML
     private TableColumn<Conge, Integer> indexColumn;
+    @FXML
+    private HBox soldeHBox; // Reference to the HBox for solde
+
     private Connection cnx;
     private final ServiceConge serviceConge = new ServiceConge();
+    private final ServiceUserSolde serviceUserSolde = new ServiceUserSolde();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -83,6 +76,7 @@ public class EmployeController implements Initializable {
             e.printStackTrace();
         }
     }
+
     private void fetchUserConges() {
         ObservableList<Conge> HisUserList = FXCollections.observableArrayList();
         String sql = "SELECT DateDebut, DateFin, TypeConge FROM conge WHERE ID_User = ? AND Statut = ?";
@@ -104,6 +98,7 @@ public class EmployeController implements Initializable {
             ex.printStackTrace();
         }
     }
+
     public void reloadUserData() {
         User currentUser = SessionManager.getInstance().getUser();
         String departement = SessionManager.getInstance().getUserDepartmentName();
@@ -130,9 +125,39 @@ public class EmployeController implements Initializable {
         } else {
             System.err.println("Image path is null or empty for user: " + currentUser);
         }
-        /*CU_ANL.setText(String.valueOf(currentUser.getSoldeAnnuel()));
-        CU_EXP.setText(String.valueOf(currentUser.getSoldeExceptionnel()));
-        CU_MAL.setText(String.valueOf(currentUser.getSoldeMaladie()));
-        CU_MAT.setText(String.valueOf(currentUser.getSoldeMaternite()));*/
+
+        populateUserSoldes(currentUser.getIdUser());
+    }
+
+    private void populateUserSoldes(int userId) {
+        List<UserSolde> soldes = serviceUserSolde.getUserSoldes(userId);
+        soldeHBox.getChildren().clear(); // Clear existing panes
+        for (UserSolde solde : soldes) {
+            Pane soldePane = createSoldePane(solde);
+            soldeHBox.getChildren().add(soldePane);
+        }
+    }
+
+    private Pane createSoldePane(UserSolde solde) {
+        Pane pane = new Pane();
+        pane.setPrefSize(130, 130);
+        pane.getStyleClass().add("paneRoundedCorners");
+
+        Label soldeTypeLabel = new Label(serviceConge.getCongeTypeName(solde.getID_TypeConge())); // Use a method to get the type name
+        soldeTypeLabel.setLayoutX(28);
+        soldeTypeLabel.setLayoutY(24);
+        soldeTypeLabel.setPrefSize(74, 36);
+        soldeTypeLabel.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
+        soldeTypeLabel.getStyleClass().addAll("FontSize-18", "RobotoItalic");
+
+        Label soldeValueLabel = new Label(String.valueOf(solde.getTotalSolde()));
+        soldeValueLabel.setLayoutX(28);
+        soldeValueLabel.setLayoutY(60);
+        soldeValueLabel.setPrefSize(74, 36);
+        soldeValueLabel.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
+        soldeValueLabel.getStyleClass().addAll("FontSize-14", "RobotoRegular");
+
+        pane.getChildren().addAll(soldeTypeLabel, soldeValueLabel);
+        return pane;
     }
 }
