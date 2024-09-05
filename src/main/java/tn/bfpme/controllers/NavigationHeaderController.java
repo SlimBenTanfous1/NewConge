71bff9bf6c3b1933716bfe8a6cf0e1b55289498c
Rayduxx <moussa.ala@esprit.tn>
2024-09-05 16:59:43 +0100

    Update NavigationHeaderController.java

--- src/main/java/tn/bfpme/controllers/NavigationHeaderController.java
+++ src/main/java/tn/bfpme/controllers/NavigationHeaderController.java
@@ -37,8 +37,6 @@ public class NavigationHeaderController implements Initializable {
     @FXML
     private Button btnRH;
     @FXML
-    private Button btnStat;
-    @FXML
     private Button test_interfaceID;
     @FXML
     private ImageView pdp2;
@@ -60,9 +58,7 @@ public class NavigationHeaderController implements Initializable {
 
         if ((userDepartmentName != null && userDepartmentName.equals("RH") && "Directeur".equals(userRoleName)) || "AdminIT".equals(userRoleName)) {
             btnRH.setVisible(true);
-            btnStat.setVisible(true);
         } else {
-            btnStat.setVisible(false);
             btnRH.setVisible(false);
         }
         admin_interface.setVisible("AdminIT".equals(userRoleName));
